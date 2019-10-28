88a5de2f955873666a152369d360c18183dbb035
GeneHFang <ghl234@nyu.edu>
2019-10-28 15:27:29 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,44 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do 
+        "My name is "
+    end
+
+    get '/hometown' do 
+        "My hometown is "
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is "
+    end
+
+    # get  '/names' do
+    #     @name = Name.all
+
+    #     erb :'name/index.html.erb'
+    # end
+
+    # get  '/hometowns' do
+    #     @hometown = Hometown.all
+
+    #     erb :'hometown/index.html.erb'
+    # end
+
+    # get  '/favorite-songs' do
+    #     @favorite_song = Song.all
+
+    #     erb :'favorite_song/index.html.erb'
+    # end
+
+
+
+
+
 end
