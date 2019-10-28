7c3bf3f159ccc4efeeac76ed3f8f5b30d798b885
TJBender <tiana.bender01@gmail.com>
2019-10-28 09:37:53 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
+require 'shotgun'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Tiana"
+    end
+
+    get '/hometown' do 
+        "My hometown is Ventura, California"
+    end
+    
+    get '/favorite-song' do
+        "My favorite song is anything by Ed Sheeran"
+    end
 end
